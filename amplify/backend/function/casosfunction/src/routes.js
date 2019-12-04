56df376724c0f8b53ecd393f2ed320e72d93c895
pepecuencar <pepecuencar@gmail.com>
2019-12-04 15:49:25 -0600

    first commit

---
+++ amplify/backend/function/casosfunction/src/routes.js
@@ -0,0 +1,112 @@
+const AWS = require('aws-sdk');
+const express = require('express');
+const uuid = require('uuid');
+
+let CASO_TABLE = "Caso";
+if(process.env.ENV && process.env.ENV !== "NONE") {
+  CASO_TABLE = CASO_TABLE + '-' + process.env.ENV;
+}
+AWS.config.update({ region: process.env.TABLE_REGION });
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const router = express.Router();
+router.get('/cases', (req, res) => {
+    const params = {
+        TableName: CASO_TABLE
+    };
+    dynamoDb.scan(params, (error, result) => {
+        if (error) {
+            res.status(400).json({ error: 'Error fetching the cases' });
+        }
+        res.json(result.Items);
+    });
+});
+router.get('/cases/:id', (req, res) => {
+    const id = req.params.id;
+    const params = {
+        TableName: CASO_TABLE,
+        Key: {
+            id
+        }
+    };
+    dynamoDb.get(params, (error, result) => {
+        if (error) {
+            res.status(400).json({ error: 'Error retrieving Employee' });
+        }
+        if (result.Item) {
+            res.json(result.Item);
+        } else {
+            res.status(404).json({ error: `Employee with id: ${id} not found` });
+        }
+    });
+});
+router.post('/cases', (req, res) => {
+    const rightNow = new Date();
+    const createdAt = rightNow.toISOString();
+    const updatedAt = createdAt;
+    const name = req.body.name;
+    const status =  "Nuevo";
+    const owner = req.body.owner;
+    const id = uuid.v4();
+    const params = {
+        TableName: CASO_TABLE,
+        Item: {
+            id,
+            createdAt,
+            name,
+            owner,
+            status,
+            updatedAt
+        },
+    };
+    dynamoDb.put(params, (error, data) => {
+        if (error) {
+            res.statusCode = 500;
+            res.json({error: error, url: req.url, body: req.body});
+        }
+        else{
+            res.json({success: 'post call succeed!', url: req.url, data: data})
+        }   
+    });
+});
+
+router.delete('/cases/:id', (req, res) => {
+    const id = req.params.id;
+    const params = {
+        TableName: CASO_TABLE,
+        Key: {
+            id
+        }
+    };
+    dynamoDb.delete(params, (error) => {
+        if (error) {
+            res.status(400).json({ error: 'Could not delete Case' });
+        }
+        res.json({ success: true });
+    });
+});
+router.put('/cases', (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const status = req.body.status;
+    const rightNow = new Date();
+    const updatedAt = rightNow.toISOString();
+    const params = {
+        TableName: CASO_TABLE,
+        Key: {
+            id
+        },
+        UpdateExpression: 'set #name = :name, #status = :status, #updatedAt = :updatedAt',
+        ExpressionAttributeNames: { '#name': 'name', '#status' : 'status', '#updatedAt' :'updatedAt'},
+        ExpressionAttributeValues: { ':name': name, ':status': status, ':updatedAt':updatedAt },
+        ReturnValues: "ALL_NEW"
+    }
+    dynamoDb.update(params, (error, result) => {
+        if (error) {
+            res.status(400).json({ error: 'Could not update Employee' });
+        }
+        res.json(result.Attributes);
+    })
+});
+module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,250 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import { Divider, Form, Grid,Header, Input, Segment, Table } from 'semantic-ui-react';
 import './App.css';
+import {v4 as uuid} from 'uuid';
+import Amplify, { API, Auth, Storage } from 'aws-amplify';
+import { S3Image, withAuthenticator } from 'aws-amplify-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
+
+function makeComparator(key, order='asc') {
+  return (a, b) => {
+    if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0; 
+
+    const aVal = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    const bVal = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (aVal > bVal) comparison = 1;
+    if (aVal < bVal) comparison = -1;
+
+    return order === 'desc' ? (comparison * -1) : comparison
+  };
 }
 
-export default App;
+class NewCase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {caseName: ''};
+    }
+
+  handleChange = (event) => {
+    let change = {};
+    change[event.target.name] = event.target.value;
+    this.setState(change);
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const user = await Auth.currentAuthenticatedUser();
+    const caseName = this.state.caseName;
+    let data = {
+      headers: {
+        'Content-Type': 'application/json'
+      }, body: {
+        owner: user.username,
+        name: caseName
+      }
+    }
+    const result = await API.post("casosapi", "/cases", data);
+    console.info('Created case with id: ' + result);
+    this.setState({caseName:''}); 
+  }
+
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>Crear un caso</Header>
+          <Input
+          type='text'
+          placeholder='Nombre del Caso'
+          icon='plus'
+          iconPosition='left'
+          action={{ content: 'Crear', onClick: this.handleSubmit }}
+          name='caseName'
+          value={this.state.caseName}
+          onChange={this.handleChange}
+          />
+        </Segment>
+      )
+    }
+}
+
+class CasesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {caso: []};
+  }
+  async componentDidMount() {
+    await this.fetchList();
+  }
+  
+  async fetchList() {
+    const response = await API.get("casosapi", "/cases");
+    this.setState({ caso: [...response] });
+  }
+  caseItems() {
+    return this.state.caso.sort(makeComparator('name')).map(caso =>
+     <Table.Row key={caso.id}>
+        <Table.Cell > <a href={'/cases/' + caso.id}> {caso.name} </a>
+        </Table.Cell>
+        <Table.Cell>{caso.status}</Table.Cell>
+      </Table.Row>
+    );
+  }
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>Mis Casos</Header>
+       <Table celled>
+        <Table.Header>
+            <Table.Row><Table.HeaderCell>Casos</Table.HeaderCell><Table.HeaderCell>Estatus</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {this.caseItems()}
+        </Table.Body>
+        </Table>
+      </Segment>
+    );
+  }
+}
+
+class S3ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { uploading: false }
+  }
+  uploadFile = async (file) => {
+    const fileName = uuid();
+    const user = await Auth.currentAuthenticatedUser();
+
+    const result = await Storage.put(
+      fileName, 
+      file, 
+      {
+        customPrefix: { public: 'uploads/' },
+        metadata: { caseid: this.props.caseId, owner: user.username }
+      }
+    );
+
+    console.log('Uploaded file: ', result);
+  }
+
+  onChange = async (e) => {
+    this.setState({uploading: true});
+    
+    let files = [];
+    for (var i=0; i<e.target.files.length; i++) {
+      files.push(e.target.files.item(i));
+    }
+    await Promise.all(files.map(f => this.uploadFile(f)));
+
+    this.setState({uploading: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <Form.Button
+          onClick={() => document.getElementById('add-image-file-input').click()}
+          disabled={this.state.uploading}
+          icon='file image outline'
+          content={ this.state.uploading ? 'Uploading...' : 'Agregar Imagen' }
+        />
+        <input
+          id='add-image-file-input'
+          type="file"
+          accept='image/*'
+          multiple
+          onChange={this.onChange}
+          style={{ display: 'none' }}
+        />
+        </div>
+    );
+  }
+}
+
+class PhotosList extends React.Component {
+  photoItems() {
+    return this.props.photos.map(photo =>
+      <S3Image 
+        key={photo.thumbnail.key} 
+        imgKey={photo.thumbnail.key.replace('public/', '')} 
+        style={{display: 'inline-block', 'paddingRight': '5px'}}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Divider hidden />
+        {this.photoItems()}
+      </div>
+    );
+  }
+}
+
+class CaseDetailsLoader extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {hasMorePhotos: true,loading: true}
+  }
+  componentDidMount() {
+  }
+  render() {
+      return (
+          <CaseDetails caso={this.state.id} 
+          />
+      );
+  }
+}
+
+
+class CaseDetails extends Component {
+  render() {
+      if (!this.props.caso) return 'Loading Case...';
+      return (
+          <Segment>
+          <Header as='h3'>{this.props.caso.name}</Header>
+          <S3ImageUpload caseId={this.props.caso.id}/>        
+          <PhotosList photos={this.props.caso.photos.items} />
+          {
+              this.props.hasMorePhotos && 
+              <Form.Button
+              onClick={this.props.loadMorePhotos}
+              icon='refresh'
+              disabled={this.props.loadingPhotos}
+              content={this.props.loadingPhotos ? 'Loading...' : 'Load more photos'}
+              />
+          }
+          </Segment>
+      )
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <Grid padded>
+          <Grid.Column>
+            <Route path="/" exact component={NewCase}/>
+            <Route path="/" exact component={CasesList} /> 
+            <Route path="/cases/:caseId" render={ () => <div><NavLink to='/'>Regresar a Mis casos</NavLink></div> }
+            />
+            <Route
+              path="/cases/:caseId"
+              render={ props => <CaseDetailsLoader id={props.match.params.caseid}/> }
+            />
+          </Grid.Column>
+        </Grid>
+      </Router>
+    );
+  }
+}  
+export default withAuthenticator(App, {includeGreetings: true});
